85d2b6f1670168f8cdbd79926fd8291b9d3512f2
Toad <codeoftoad@gmail.com>
2024-08-17 22:21:22 -0400

    Refactor template configuration

--- python-manim/template_toad_manim.py
+++ python-manim/template_toad_manim.py
@@ -2,19 +2,21 @@ from manim import *
 
 
 class ToadTemplate(Scene):
+    def _config(self, bg_color='#131313', grid=True, font='Arial'):
+        config.default_font = font
+        self.camera.background_color = bg_color
+        if grid is True:
+            self.add(NumberPlane(
+                background_line_style={
+                    "stroke_color": WHITE, "stroke_width": 1, "stroke_opacity": 0.1
+                },
+                axis_config={
+                    "stroke_color": WHITE, "stroke_width": 1, "stroke_opacity": 0.1
+                }
+            ))
 
     def construct(self):
         # ------------------------------ CONFIG -------------------------------
-        config.default_font = 'Arial'
-        self.camera.background_color = '#131313'
-        self.add(NumberPlane(
-            background_line_style={
-                "stroke_color": WHITE, "stroke_width": 1, "stroke_opacity": 0.1
-            },
-            axis_config={
-                "stroke_color": WHITE, "stroke_width": 1, "stroke_opacity": 0.1
-            }
-        ))
+        self._config()
         # ---------------------------------------------------------------------
         pass
-
