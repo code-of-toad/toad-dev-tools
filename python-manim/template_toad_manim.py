14420bb2ade640b8989611778a4366b3e84ac5d0
Toad <codeoftoad@gmail.com>
2024-08-18 17:25:33 -0400

    Add screen resolution options

--- python-manim/template_toad_manim.py
+++ python-manim/template_toad_manim.py
@@ -1,11 +1,23 @@
 from manim import *
 
+"""
+Uncomment the next 4 lines for vertical resolution.
+"""
+# config.pixel_width  = 1080
+# config.pixel_height = 1920
+# config.frame_width  = 4.5
+# config.frame_height = 8.0
 
 class ToadTemplate(Scene):
-    def _config(self, bg_color='#131313', grid=True, font='Arial'):
-        config.default_font = font
+    def construct(self):
+        # ------------------------------ CONFIG -------------------------------
+        self._config()
+        # ---------------------------------------------------------------------
+        pass
+
+    def _config(self, bg_color='#131313', grid=True):
         self.camera.background_color = bg_color
-        if grid is True:
+        if grid:
             self.add(NumberPlane(
                 background_line_style={
                     "stroke_color": WHITE, "stroke_width": 1, "stroke_opacity": 0.1
@@ -15,8 +27,3 @@ class ToadTemplate(Scene):
                 }
             ))
 
-    def construct(self):
-        # ------------------------------ CONFIG -------------------------------
-        self._config()
-        # ---------------------------------------------------------------------
-        pass
