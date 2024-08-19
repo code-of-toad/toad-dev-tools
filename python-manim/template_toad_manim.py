22123dca32bb1529749b3c787d65a1ebca1ff6b4
Toad <codeoftoad@gmail.com>
2024-08-19 00:48:27 -0400

    Update manim template

--- python-manim/template_toad_manim.py
+++ python-manim/template_toad_manim.py
@@ -6,8 +6,8 @@ Uncomment the next 4 lines for vertical resolution.
 """
 # config.pixel_width  = 1080
 # config.pixel_height = 1920
-# config.frame_width  = 4.5
-# config.frame_height = 8.0
+# config.frame_width  = 9.0
+# config.frame_height = 16.0
 
 class ToadTemplate(Scene):
     def construct(self):
