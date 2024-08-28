f9766ddc66fe0d55de617daa3f679e7893ea0cb9
toad <codeoftoad@gmail.com>
2024-08-28 03:19:57 -0400

    Update nvim config

--- neovim-config/init.lua
+++ neovim-config/init.lua
@@ -13,7 +13,20 @@ vim.opt.signcolumn = "yes"       -- Always show the sign column
 
 -- Key mappings
 vim.g.mapleader = " "            -- Set leader key to space
-vim.api.nvim_set_keymap('n', '<leader>w', ':w<CR>', { noremap = true, silent = true })  -- Save with Space+w
+
+-- Ensure that `packer.nvim` is bootstrapped
+local ensure_packer = function()
+  local fn = vim.fn
+  local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+  if fn.empty(fn.glob(install_path)) > 0 then
+    fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
+    vim.cmd [[packadd packer.nvim]]
+    return true
+  end
+  return false
+end
+
+local packer_bootstrap = ensure_packer()
 
 -- Plugins (using packer.nvim as an example)
 require('packer').startup(function()
@@ -24,16 +37,29 @@ require('packer').startup(function()
   use 'nvim-treesitter/nvim-treesitter'   -- Treesitter for better syntax highlighting
   use 'nvim-telescope/telescope.nvim'     -- Fuzzy finder
   use 'nvim-lua/plenary.nvim'             -- Common utilities for plugins
-  use 'nvim-telescope/telescope-fzf-native.nvim' -- FZF sorter for Telescope
+  use { 'nvim-telescope/telescope-fzf-native.nvim', run='make' }  -- FZF sorter for Telescope
   use 'nvim-lualine/lualine.nvim'         -- Statusline
   use 'nvim-tree/nvim-tree.lua'           -- File explorer
   use 'psf/black'                         -- Python code formatter
   use 'mfussenegger/nvim-dap'             -- Debugger
-  use 'navarasu/onedark.nvim'
+  use 'navarasu/onedark.nvim'             -- Color scheme: Atom One Dark
+  use 'numToStr/Comment.nvim'
+
+  if packer_bootstrap then
+    require('packer').sync()
+  end
 end)
 
 -- LSP settings for Python (using pyright)
-require('lspconfig').pyright.setup{}
+require('lspconfig').pyright.setup({
+  settings = {
+    python = {
+      analysis = {
+        typeCheckingMode = 'off',
+      },
+    },
+  },
+})
 
 -- Treesitter setup
 require('nvim-treesitter.configs').setup {
@@ -105,28 +131,62 @@ vim.g.python3_host_prog = '/usr/bin/python3'  -- Set Python 3 interpreter
 
 -- Color scheme
 require('onedark').setup {
-    style = 'dark', -- Set the theme style do dark
-    colors = {
-        bg = '#1c1c1c', -- Set a custom darker bg color
-    },
-    highlights = {
-        Normal = {bg = '#1c1c1c' }  -- Ensure the normal background uses the custom color
-    },
-    transparent = false, -- Do not use a transparent background
-    term_colors = true,  -- Change terminal colors as well
-    ending_tildes = false, -- Show end-of-buffer tildes
-    cmp_itemkind_reverse = false, -- Reverse item kinds in completion menu
-    code_style = {
-        comments = 'italic',
-        keywords = 'bold',
-        functions = 'italic,bold',
-        strings = 'none',
-        variables = 'none'
-    },
-    diagnostics = {
-        darker = true, -- Darker colors for diagnostic
-        undercurl = true, -- Use undercurl instead of underline for diagnostics
-        background = false, -- Use background color for virtual text
-    },
+  style = 'dark', -- Set the theme style do dark
+  colors = {
+    bg = '#1c1c1f', -- Set a custom darker bg color
+  },
+  highlights = {
+    Normal = {bg = '#1c1c1f' }  -- Ensure the normal background uses the custom color
+  },
+  transparent = false, -- Do not use a transparent background
+  term_colors = true,  -- Change terminal colors as well
+  ending_tildes = false, -- Show end-of-buffer tildes
+  cmp_itemkind_reverse = false, -- Reverse item kinds in completion menu
+  code_style = {
+    comments = 'none',
+    keywords = 'none',
+    functions = 'bold',
+    strings = 'none',
+    variables = 'none'
+  },
+  diagnostics = {
+    darker = true, -- Darker colors for diagnostic
+    undercurl = true, -- Use undercurl instead of underline for diagnostics
+    background = false, -- Use background color for virtual text
+  },
 }
 require('onedark').load()
+
+-- Comment out lines
+require('Comment').setup({
+  -- Add any custom configuration here
+  padding = true, -- Add a space between comment and the line
+  sticky = true,  -- Keeps the cursor in place after commenting
+  mappings = {
+    -- operator-pending mapping
+    basic = true,    -- `gcc` to comment a line
+    extra = true,    -- `gbc` to block comment
+    extended = false,
+  },
+})
+
+-- KEY MAPPINGS
+vim.api.nvim_set_keymap('n', '<leader>w', ':w<CR>', { noremap = true, silent = true })     -- Save with Space + w
+vim.api.nvim_set_keymap('n', '<leader>e', ':<Up><CR>', { noremap = true, silent = true })  -- Execute last nvim command with Space + e
+-- Keybinding for commenting line(s)
+vim.keymap.set('n', '<C-_>', 'gcc', { noremap = true, silent = true })
+vim.keymap.set('v', '<C-_>', 'gc',  { noremap = true, silent = true })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
